81a77cc02892ab94d1975d1775a4194b38ae1384
semyon422 <semyon422@gmail.com>
2017-03-06 21:13:27 +0300

    New cursor and FPS counter.

--- src/uiBase/cursor.lua
+++ src/uiBase/cursor.lua
@@ -1,47 +1,35 @@
 local cursor = ui.classes.UiObject:new()
 
-local templates = {
-	{r = 8, color = {255, 255, 255}, mode = "line"},
-	{r = 4, color = {0, 0, 0}, mode = "fill"},
-}
-local circles = {}
-local pos = loveio.output.Position:new({ratios = {0}, resolutions = {{0, 0}}})
+local image = love.graphics.newImage("res/cursor.png")
+local pos = loveio.output.Position:new({ratios = {1}, resolutions = {{1, 1}}})
+local wh = 0.1
 
 cursor.load = function(self)
 	love.mouse.setVisible(false)
-	for index, template in ipairs(templates) do
-		table.insert(circles, loveio.output.classes.Circle:new({
-			x = 0, y = 0, r = template.r,
-			mode = template.mode,
-			color = template.color,
-			layer = 1001,
-			pos = pos
-		}):insert(loveio.output.objects))
-	end
+	self.drawableBox = loveio.output.classes.DrawableBox:new({
+		x = 0, y = 0, w = wh, h = wh,
+		drawable = image,
+		layer = 1001,
+		pos = pos
+	}):insert(loveio.output.objects)
 	
 	loveio.input.callbacks.mousepressed.cursor = function(mx, my)
-		for index, circle in ipairs(circles) do
-			circle.x = mx
-			circle.y = my
-		end
+		self.drawableBox.x = pos:X2x(mx, true) - wh/2
+		self.drawableBox.y = pos:Y2y(my, true) - wh/2
 	end
 	loveio.input.callbacks.mousemoved.cursor = function(mx, my)
-		for index, circle in ipairs(circles) do
-			circle.x = mx
-			circle.y = my
-		end
+		self.drawableBox.x = pos:X2x(mx, true) - wh/2
+		self.drawableBox.y = pos:Y2y(my, true) - wh/2
 	end
 	loveio.input.callbacks.mousereleased.cursor = function(mx, my)
-		for index, circle in ipairs(circles) do
-			circle.x = mx
-			circle.y = my
-		end
+		self.drawableBox.x = pos:X2x(mx, true) - wh/2
+		self.drawableBox.y = pos:Y2y(my, true) - wh/2
 	end
 end
 
-cursor.unload = function()
-	for index, circle in ipairs(circles) do
-		circle:remove()
+cursor.unload = function(self)
+	if self.drawableBox then
+		self.drawableBox:remove()
 	end
 	loveio.input.callbacks.mousepressed.cursor = nil
 	loveio.input.callbacks.mousemoved.cursor = nil
